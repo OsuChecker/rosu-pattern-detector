4eab886e46535399bb4b95408dadd4dc6c850509
mcheva06 <50545507+Glubus@users.noreply.github.com>
2025-01-23 19:25:19 +0100

    Refactor imports and improve `get_map` mode handling.
    Reorganized imports for better structure and readability. Updated `get_map` to handle non-Mania game modes by returning an empty string early, preventing unnecessary processing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 mod mania;
 
-use std::cmp::PartialEq;
-use std::collections::{BTreeMap, HashMap};
-use std::iter::Map;
 use pyo3::prelude::*;
+use reqwest::blocking;
 use rosu_map;
-use rosu_map::Beatmap;
 use rosu_map::section::hit_objects::{HitObject, HitObjectKind};
 use rosu_map::section::timing_points::TimingPoint;
-use reqwest::blocking;
-use std::error::Error;
+use rosu_map::section::Section::Mania;
+use rosu_map::Beatmap;
 use serde_json::json;
 use serde_json::Value::Null;
+use std::cmp::PartialEq;
+use std::collections::{BTreeMap, HashMap};
+use std::error::Error;
+use std::iter::Map;
 
 /// Module principal à exposer pour Python
-
-
 
 pub fn download_file(url: &str) -> Result<String, Box<dyn Error>> {
     let response = blocking::get(url)?;
@@ -26,34 +25,33 @@ pub fn download_file(url: &str) -> Result<String, Box<dyn Error>> {
     }
 }
 
-
 #[pyfunction]
-fn get_map(url : &str) -> PyResult<String>{
+fn get_map(url: &str) -> PyResult<String> {
     let path = download_file(url).unwrap();
     let map = rosu_map::from_str::<Beatmap>(&path).unwrap();
-    let timing_point = map.control_points.timing_points;
+    if (map.mode != rosu_map::section::general::GameMode::Mania) {
+        let timing_point = map.control_points.timing_points;
 
-    let notes = mania::transform_ho_to_mania_notes(map.hit_objects);
-    let mut mesure = mania::group_notes_by_measures(notes, timing_point);
-    let (jack_count, jumpstream_count, singlestream_count, handstream_count) =
-        mania::analyze_patterns_by_measures_advanced(&mut mesure);
+        let notes = mania::transform_ho_to_mania_notes(map.hit_objects);
+        let mut mesure = mania::group_notes_by_measures(notes, timing_point);
+        let (jack_count, jumpstream_count, singlestream_count, handstream_count) =
+            mania::analyze_patterns_by_measures_advanced(&mut mesure);
 
-    // Convertir les résultats en JSON
-    let result_json = json!({
-        "jack": jack_count,
-        "jumpstream": jumpstream_count,
-        "singlestream": singlestream_count,
-        "handstream": handstream_count
-    });
-
+        // Convertir les résultats en JSON
+        let result_json = json!({
+            "jack": jack_count,
+            "jumpstream": jumpstream_count,
+            "singlestream": singlestream_count,
+            "handstream": handstream_count
+        });
+    } else {
+        return Ok("".to_string());
+    }
     Ok(result_json.to_string())
 }
-
 
 #[pymodule]
 fn pdetector(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_map, m)?)?;
     Ok(())
 }
-
-
